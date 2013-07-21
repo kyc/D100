a9535a3afd03b9bd4b2393b3c1176a3ca6b77e6d
kyc <keyongcheng@gmail.com>
2013-07-21 22:28:00 +0800

    new line

--- record.rb
+++ record.rb
@@ -14,52 +14,91 @@ end
 scheduler = Rufus::Scheduler.start_new
 
 
-
-scheduler.cron '*/3 17 * * 1-5' do
-  puts gen_cmd(2.5,'teacup')
-end
-
-
 # 风波里的茶杯
-scheduler.cron '30 7 * * 1-5' do
+scheduler.cron '0 8 * * 1-5' do
   system(gen_cmd(2.5,'teacup'))
 end
 
 # 开心快活人
-scheduler.cron '0 10 * * 1-5' do
-  system(gen_cmd(3,'happy'))
+scheduler.cron '30 10 * * 1-5' do
+  system(gen_cmd(2.5,'happy'))
 end
 
-#事有QK
+#政本清源
 scheduler.cron '0 13 * * 1-5' do
-  system(gen_cmd(2,'QK'))
+  system(gen_cmd(2,'zbqy'))
+end
+
+#焦点新闻
+scheduler.cron '0 15 * * 1-5' do
+  system(gen_cmd(1,'jiaodian'))
 end
 
 #为食show
-scheduler.cron '0 15 * * 1-5' do
+scheduler.cron '00 16 * * 1-5' do
   system(gen_cmd(1.5,'show'))
-end
-
-#世界公民
-scheduler.cron '30 16 * * 1-5' do
-  system(gen_cmd(1.5,'world'))
 end
 
 
 #十级自由风
-scheduler.cron '0 18 * * 1-5' do
-  system(gen_cmd(2,'free'))
+scheduler.cron '30 17 * * 1-5' do
+  system(gen_cmd(2.5,'free'))
 end
 
 
-#音乐节目
+#怒汉系列
 scheduler.cron '0 20 * * 1-5' do
-  system(gen_cmd(2,'music'))
+  system(gen_cmd(2,'luhan'))
+end
+
+#人间锦言
+scheduler.cron '0 22 * * 1-5' do
+  system(gen_cmd(1,'renjian'))
 end
 
 #霎时冲动
-scheduler.cron '0 22 * * 1-5' do
+scheduler.cron '0 23 * * 1-5' do
   system(gen_cmd(2,'impulse'))
+end
+
+#风波里的茶杯 周六
+scheduler.cron '0 8 * * 6' do
+  system(gen_cmd(2,'teacup'))
+end
+
+#龙凤大茶楼
+scheduler.cron '0 10 * * 6' do
+  system(gen_cmd(3,'lf'))
+end
+
+#琪文遁甲
+scheduler.cron '0 13 * * 6' do
+  system(gen_cmd(3,'fs'))
+end
+
+#音乐真情人
+scheduler.cron '0 18 * * 6' do
+  system(gen_cmd(3,'music'))
+end
+
+#好友感觉
+scheduler.cron '0 21 * * 6' do
+  system(gen_cmd(3,'friend'))
+end
+
+#sunday还可以
+scheduler.cron '0 10 * * 7' do
+  system(gen_cmd(3,'sunday'))
+end
+
+#对冲人生路
+scheduler.cron '0 13 * * 7' do
+  system(gen_cmd(2,'road'))
+end
+
+#女皇头
+scheduler.cron '0 21 * * 7' do
+  system(gen_cmd(3,'queen'))
 end
 
 scheduler.join 
